8cd87d8e9c2ab754c6774a7a908a384b063d00c5
calberto-payall <cserrano@payall.com.ve>
2023-05-22 08:44:56 -0400

    first commit

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,27 +2,42 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     /**
-     * @Route ("/")
+     * @Route ("/", name="app_vinyl_index")
      */
     public function indexAction(): Response
     {
-        die('vinylController');
+        $tracks = [
+            ['song' => 'Gangsta\' Paradise', 'artist' => 'Coolio'],
+            ['song' => 'In the end', 'artist' => 'Linkin Park'],
+            ['song' => 'Losing my Religion', 'artist' => 'REM'],
+            ['song' => 'Like a stone', 'artist' => 'Audioslave'],
+            ['song' => 'Did my time', 'artist' => 'Korn'],
+            ['song' => 'Feel', 'artist' => 'Robbie Williams']
+        ];
+
+        return $this->render('vinyl/index.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     /**
-     * @Route ("/browse/{slug}")
+     * @Route ("/browse/{slug}", name="app_vinyl_browse")
      */
     public function browseAction($slug = null): Response
     {
-        $title = $slug ? 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true) : 'All Genre';
-        return new Response($title);
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
     }
 
 }

6d8b50ccb74f9329751325f78c864bed9978dc95
calberto-payall <cserrano@payall.com.ve>
2023-05-22 10:41:54 -0400

    Adding Webpack Encore

--- src/Controller/SongApiController.php
+++ src/Controller/SongApiController.php
@@ -2,7 +2,29 @@
 
 namespace App\Controller;
 
-class SongApiController
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongApiController extends AbstractController
 {
+    /**
+     * @Route ("/api/songs/{id<\d+>}", name="app_songapi_getsong", methods={"GET"} )
+     */
+    public function getSongAction(int $id, LoggerInterface $logger): Response
+    {
+        //TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Zombie',
+            'url' => ' https://www.youtube.com/watch?v=6Ejga4kJUts'
+        ];
+        $logger->info('Returning API response for song {song}',[
+            'song' => $id,
+        ]);
+        return new JsonResponse($song);
+    }
 
 }
